90ed483a40ab4ee6a410dd82894c0a5b6bc41d1b
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2025-12-17 19:16:25 +0530

    project updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
       <div className=' min-h-screen    '>
         
 
-            <div className=' p-8 bg-black w-full flex justify-end'>
-                <div id='heading' className='flex text-white  text-xl md:font-light font-light md:text-2xl ' ><div>Currency</div><div>-Converter💱</div></div>
+            <div className=' p-3 bg-gradient-to-r from-[#0b1f24] via-[#0f3d3e] to-[#14532d] w-full flex justify-between'>
+                <div id='heading' className='flex text-blue-100  text-lg md:font-light font-black md:text-2xl sm:pr-4' ><div>Currency</div><div>-Converter💱</div></div>
                              <video
     autoPlay
     loop
@@ -20,12 +20,12 @@ function App() {
     preload="auto"
     className="fixed inset-0 w-full h-full object-cover -z-10"
   >
-    <source src="/coins.mp4" type="video/mp4" />
+    <source src="/waves.mp4" type="video/mp4" />
   </video>
         
                           <div>
                                  <div
-                                 className="fixed top-9.5 left-6 z-50 cursor-pointer space-y-1"
+                                 className=" top-5 left-6 z-50 cursor-pointer space-y-1 pl-14 pt-1 md:pl-100 "
                                   
                                                                                          >
                                      <span className="block w-7 h-1 bg-white"></span>
@@ -40,10 +40,40 @@ function App() {
 
             </div>
 
-         <div className='bg-white h-12 w-full flex justify-around items-center font-bold'>
-                  <div id='animatetwo' className='font-black'>Home</div>
-                  <div id='animatethree' className='font-black'>About</div>
-                  <div id='animatefour' className='text-red-500 font-mono text-xl bg-black rounded-sm p-1 ring-1 ring-offset-red-200' >SignIn</div>
+         <div className='bg-gradient-to-r from-[#0b1f24] via-[#0f3d3e] to-[#14532d] h-8 border-black border-y-2  w-full flex justify-between md:flex md:justify-around items-center md:font-bold py-6 px-4'>
+                  <div id='animatetwo' className='font-medium  bg-black px-2 md:w-1/20 rounded-sm border border-yellow-100 text-white'><a href="">Home</a></div>
+                  <div id='animatethree' className='font-medium bg-black text-white border border-blue-500 px-2 rounded-sm'><a href="">About</a></div>
+                  <div id='animatefour' className='text-red-300 font-medium text-sm     bg-black hover:bg-gray-300 hover:border hover:border-black cursor-pointer hover:text-black rounded-sm p-1  ' ><a href="">Sign-In/Up</a></div>
+
+         </div>
+
+         <div id='ani2' className='flex justify-center  py-10 text-sm font-extralight'>
+             <div className='flex px-4'> <div id='animate1'><h1>W</h1></div>
+                   <div id='animate2'><h1>H</h1></div>
+                   <div id='animate3'><h1>E</h1></div>
+                   <div id='animate4'><h1>R</h1></div>
+                   <div id='animate5'><h1>E</h1></div>
+             </div>
+              <div className='flex'> <div><h1 id='animate6'>I</h1></div>
+                    <div id='animate7'><h1>N</h1></div>
+                    <div id='animate8'><h1>N</h1></div>
+                    <div id='animate9'><h1>O</h1></div>
+                    <div id='animate10'><h1>V</h1></div>
+                    <div id='animate11'><h1>A</h1></div>
+                    <div id='animate12'><h1>T</h1></div>
+                    <div id='animate13'><h1>I</h1></div>
+                    <div id='animate14'><h1>O</h1></div>
+                    <div id='animate15'><h1>N</h1></div>
+              <div className='flex px-4'> <div id='animate16'><h1>S</h1></div>
+                    <div id='animate17'><h1>T</h1></div>
+                    <div id='animate18'><h1>A</h1></div>
+                    <div id='animate19'><h1>R</h1></div>
+                    <div id='animate20'><h1>T</h1></div>
+                    <div id='animate21'><h1>S</h1></div>
+                    <div id='animate22'><h1>!</h1></div>
+              </div>
+            </div>
+
 
          </div>
 
@@ -56,18 +86,16 @@ function App() {
 
 
 
-
-
-        <div className='flex justify-center min-h-[400px] pt-10'>
-            <div className=' bg-gray-50 min-h-100   rounded-xl w-full sm:w-full md:w-[70%] lg:w-[30%] ring-1 ring-offset-5 ring-blue-400 ring-offset-black'>
-                <div className='text-center p-6 '><h1 className='text-stone-700 font-semibold text-2xl '>CURRENCY CONVERTER</h1></div>
+        <div className='flex justify-center min-h-[400px] pt-6'>
+            <div id='ani1' className=' bg-green-50 min-h-30   rounded-xl w-[90%] sm:w-full md:w-[70%] lg:w-[30%] ring-1 ring-offset-5 ring-green-200 ring-offset-black'>
+                <div className='text-center p-6 '><h1 className='text-stone-700 font-black text-2xl '>CURRENCY CONVERTER</h1></div>
                 <div className='flex justify-center'> <hr className='w-[90%]'/></div><br />
                 <div className='pt-6 pb-6 pr-7 text-center bg-green-50 font-semibold border border-green-50 rounded-2xl 'id='animate'>AMOUNT:  <label className='bg-white'><input type='number' placeholder='  Enter Amount Here ' min={1} className='border-3 font-medium border-black rounded-sm'/></label></div>
                 <div className='flex justify-center p-4'>
-                              <div className='pr-20'><label className='flex bg-green-200 rounded-sm font-light border-black border-1'>FROM: <div className=' border-black border-2 rounded-sm bg-yellow-100 text-blue-500'><select><option >USD</option><option >INR</option><option >EUR</option><option >GBP</option><option >AUD</option></select></div></label></div>
+                              <div className='pr-20'><label className='flex bg-green-200 rounded-sm font-light border-black border'>FROM: <div className=' border-black border-2 rounded-sm bg-yellow-100 text-blue-500'><select><option >USD</option><option >INR</option><option >EUR</option><option >GBP</option><option >AUD</option></select></div></label></div>
                               <div ><label className='flex bg-green-200 rounded-sm border-black border'>TO:   <div className='border-2 border-black  rounded-sm bg-yellow-100 font-light  text-blue-500'><select><option >INR</option><option >USD</option><option >EUR</option><option >GBP</option><option >AUD</option></select></div></label></div>
                 </div>
-                <div className='text-center pt-10 pl-5 '><button className='bg-black text-white rounded-2xl text-base p-4   font-semibold  px-6 hover:ring hover:ring-offset-2 hover:ring-red-200 hover:text-white hover:from-blue-400 hover:to-purple-500 hover:bg-linear-to-r transition-all duration-300 transform hover:scale-105 shadow-2xl ring ring-offset-1 ring-blue-500 hover:cursor-pointer '>CONVERT</button></div>
+                <div className='text-center pt-10 pl-5 '><button className='bg-black text-white rounded-2xl text-base py-4   font-semibold  px-3 hover:ring hover:ring-offset-2 hover:ring-red-200 hover:text-white hover:from-blue-400 hover:to-purple-500 hover:bg-linear-to-r transition-all duration-300 transform hover:scale-105 shadow-2xl ring ring-offset-1 ring-blue-500 hover:cursor-pointer '>CONVERT</button></div>
 
             </div>
 
